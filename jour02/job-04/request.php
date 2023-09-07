e1ff890e63a09580ae061d39546dea36b454f82d
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-07 21:39:21 +0200

    fix error DateTime job04

--- jour02/job-04/request.php
+++ jour02/job-04/request.php
@@ -4,17 +4,19 @@ function logDB() {
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $bdd;
 }
-function my_insert_student(string $email, string $fullname, string $genre, int $grade_id, string $birthdate): bool {
+function my_insert_student(string $email, string $fullname, string $genre, int $grade_id, DateTime $birthdate): bool {
     logDB();
     $query = logDB()->prepare('INSERT INTO student (email, fullname, gender, grade_id, birthdate) VALUES (:email, :fullname, :genre, :grade_id, :birthdate)');
-    $query->execute(['email' => $email, 'fullname' => $fullname, 'genre' => $genre, 'grade_id' => $grade_id, 'birthdate' => $birthdate]);
+    $birthdateStr = $birthdate->format('Y-m-d');
+    $query->execute(['email' => $email, 'fullname' => $fullname, 'genre' => $genre, 'grade_id' => $grade_id, 'birthdate' => $birthdateStr]);
     return true;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['student-mail'];
     $fullname = $_POST['student-fullname'];
-    $birthdate = $_POST['student-birthdate'];
+    $dateStr = $_POST['student-birthdate'];
+    $birthdate = DateTime::createFromFormat('Y-m-d', $dateStr);
     $gender = $_POST['student-gender'];
     $grade = $_POST['student-grade'];
     if (!empty($email) || !empty($fullname) || !empty($birthdate) || !empty($gender) || !empty($grader)) {
